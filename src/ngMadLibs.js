93662a734adc81a67b59507de71cbc059849dc85
Ben Boyle <benjamins.boyle@gmail.com>
2014-05-22 15:09:49 +1000

    add a controller
    - added mab libs controller
    - removed ng-init attributes from view

---
+++ src/ngMadLibs.js
@@ -0,0 +1,12 @@
+// http://www.thinkful.com/courses/ANG-001/unit/1/lesson/3/assignment/3
+angular.module( 'madLibs', [] )
+.controller( 'madLibsController', function( $scope ) {
+	$scope.femaleName = 'Sue';
+	$scope.jobTitle = 'Teacher';
+	$scope.tediousTask = 'playground duty';
+	$scope.dirtyTask = 'math';
+	$scope.uselessSkill = '‘pity da foo’';
+	$scope.obnoxiousCelebrity = 'Mr. T';
+	$scope.adjective = 'foo’ pitying';
+	$scope.hugeNumber = '1000 times';
+});
